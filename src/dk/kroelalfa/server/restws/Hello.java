c48cc532c969b379540571b90054ce68c1dd2a3d
Mikkel Vestergaard <baskernet@gmail.com>
2015-12-01 15:50:00 +0100

    Added support for JSON

--- src/dk/kroelalfa/server/restws/Hello.java
+++ src/dk/kroelalfa/server/restws/Hello.java
@@ -5,6 +5,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 // Plain old Java Object it does not extend as class or implements 
 // an interface
 
@@ -18,28 +21,35 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class Hello {
 
-	//something changed.
-	
-  // This method is called if TEXT_PLAIN is request
-  @GET
-  @Produces(MediaType.TEXT_PLAIN)
-  public String sayPlainTextHello() {
-    return "Hello Jersey";
-  }
+	//Returns a JSON string
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String sayJsonTextHello() throws JSONException {
+		JSONObject job = new JSONObject();
+		job.append("Say", "Hello");
+		return job.toString();
+	}
 
-  // This method is called if XML is request
-  @GET
-  @Produces(MediaType.TEXT_XML)
-  public String sayXMLHello() {
-    return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
-  }
+	// This method is called if TEXT_PLAIN is request
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Hello Jersey";
+	}
 
-  // This method is called if HTML is request
-  @GET
-  @Produces(MediaType.TEXT_HTML)
-  public String sayHtmlHello() {
-    return "<html> " + "<title>" + "Hello Jersey" + "</title>"
-        + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
-  }
+	// This method is called if XML is request
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayXMLHello() {
+		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+	}
+
+	// This method is called if HTML is request
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello() {
+		return "<html> " + "<title>" + "Hello Jersey" + "</title>" + "<body><h1>" + "Hello Jersey" + "</body></h1>"
+				+ "</html> ";
+	}
 
 }
